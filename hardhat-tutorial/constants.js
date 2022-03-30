279bcef4e4db6d6ecca7135b7399eb8adc681ce9
Spring Zhang <spring.okay@gmail.com>
2022-03-30 16:45:07 +0800

    Added DAO tutorial

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x0D7e183c94abAD4bbfCaa7D720b0FF0Fe6C873fd";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
